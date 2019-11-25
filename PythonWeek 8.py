403276cdbe70332d8e3e970865aa58572ca20785
Ahmadam37 <Ahmadam37@hotmail.com>
2019-11-25 15:36:59 +0300

    day 49

--- PythonWeek 8.py
+++ PythonWeek 8.py
@@ -1,16 +1,52 @@
-def myfunction():
+# def myfunction():
+#     x = 300
+#     print(x)
+#
+# myfunction()
+#
+#
+#
+# def myfunction():
+#     x = 300
+#     def myinestedFun():
+#         print(x)
+#     myinestedFun()
+#
+#
+# myfunction()
+#
+#
+#
+#
+#
+# x = 300
+#
+# def myfunc():
+#     x = 200
+#     print(x)
+#
+# myfunc()
+#
+# print(x)
+
+
+
+def func():
+    global x
     x = 300
-    print(x)
 
-myfunction()
+func()
 
-
-
-def myfunction():
-    x = 300
-    def myinestedFun():
-        print(x)
-    myinestedFun()
+print(x)
 
 
-myfunction()
+y = 300
+def func():
+    global y
+    y = 200
+
+func()
+
+print(y)
+
+
