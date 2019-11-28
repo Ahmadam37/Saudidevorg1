ac7321a4e830006a169a086e956f1af70ee15e85
Ahmadam37 <Ahmadam37@hotmail.com>
2019-11-28 20:08:01 +0300

    day 66

--- String Formatting.py
+++ String Formatting.py
@@ -13,3 +13,22 @@ sum = price * item + quantity
 myOrder = "I want {} pieces of item number {} for {:.2f} dollars, and the sum is {:.2f}."
 print(myOrder.format(quantity,item,price,sum))
 
+
+print("Enter Your Age")
+age = input()
+print("Enter Your Name")
+name = input()
+txt= "His name is {1}. {1} is {0} years old."
+print(txt.format(age,name))
+
+
+
+myOrder = "I have a {carname}, it is a {model}"
+print("Enter Car Name")
+Car = input()
+print("Enter the car model")
+Model = input()
+print(myOrder.format(carname = Car, model = Model))
+
+
+
